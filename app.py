ebfb08a7a63bce65981ac9a57976f2fceea13d93
etamilselvan2022 <etamilselvan2022@gmail.com>
2022-06-30 09:09:34 +0530

    updated

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "Start Machine Learning Project"
+    return "Love u Prema.I miss u too.Don't forget to had ur breakfast and lunch.Take care of urself.Bye!..."
 
 if __name__=="__main__":
     app.run(debug=True)    
