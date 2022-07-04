b425f06aa75a4f794a0f16d8f8d53eee2e178796
etamilselvan2022 <etamilselvan2022@gmail.com>
2022-07-04 10:22:49 +0530

    'latest

--- demo.py
+++ demo.py
@@ -2,13 +2,19 @@ from housing.pipeline.pipeline import Pipeline
 from housing.exception import HousingException
 from housing.logger import logging
 from housing.config.configuration import Configuration
+from housing.component.data_validation import DataValidation
+from housing.component.data_ingestion import DataIngestion
 
 def main():
     try:
-        #pipeline=Pipeline()
-        #pipeline.run_pipeline()
-        v=Configuration().get_data_validation_config()
-        print(v)
+        pipeline=Pipeline()
+        pipeline.run_pipeline()
+        #data_ingestion_artifact=DataIngestion(Configuration().get_data_ingestion_config()).initiate_data_ingestion()
+        #data_validation_config=Configuration().get_data_validation_config()
+        #val=DataValidation(data_validation_config,data_ingestion_artifact).is_train_test_file_exists()
+        #print(f"data_validation_config:{data_validation_config}")
+        #print(f"\n \n data_ingestion_artifact:{data_ingestion_artifact}")
+        #print(val)
     except Exception as e:
         logging.error(f'{e}')
         print(e)
